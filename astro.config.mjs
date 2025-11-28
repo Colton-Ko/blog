b20222f7b0f13543b8a25c1f14dce3a5ce1f705e
Eva <156052123+gideon-eva1201@users.noreply.github.com>
2025-11-28 12:28:50 +0800

    (fix) mermaid now render in clientside

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
-import rehypeMermaid from 'rehype-mermaid';
 import rehypeRaw from 'rehype-raw';
 import rehypeHighlight from 'rehype-highlight';
 import remarkDirective from 'remark-directive';
@@ -11,7 +10,6 @@ import remarkDesmos from './src/plugins/remark-desmos.mjs';
 import remarkHighlight from './src/plugins/remark-highlight.mjs';
 import remarkSubSup from './src/plugins/remark-subsup.mjs';
 import rehypePreserveMermaid from './src/plugins/rehype-preserve-mermaid.mjs';
-import reHypeMermaidClass from './src/plugins/rehype-mermaid-class.mjs';
 
 // https://astro.build/config
 export default defineConfig({
@@ -35,8 +33,6 @@ export default defineConfig({
             }],
             rehypeHighlight,
             rehypePreserveMermaid, // Add before mermaid to wrap and preserve source
-            [rehypeMermaid, { strategy: 'img-svg' }],
-            reHypeMermaidClass,
         ],
     },
 });
